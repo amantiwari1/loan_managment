016c1435274bb1f3068469e3ef0a913dcfc0ecbb
Aman Tiwari <amantiwari78632@gmail.com>
2022-02-20 01:25:14 +0530

    fix: error

--- app/core/components/Table.tsx
+++ app/core/components/Table.tsx
@@ -72,7 +72,7 @@ export function SelectColumnFilter({
     preFilteredRows.forEach((row) => {
       options.add(row.values[id])
     })
-    return [...options.values()]
+    return [...(options.values() as any)]
   }, [id, preFilteredRows])
 
   // Render a multi-select box
@@ -141,20 +141,6 @@ export function StatusPillCell({ value }) {
     <Tag colorScheme={StatusData[value?.id ? "true" : "false"]?.color}>
       {StatusData[value?.id ? "true" : "false"]?.title}
     </Tag>
-  )
-}
-
-export function AvatarCell({ value, column, row }) {
-  return (
-    <div className="flex items-center">
-      <div className="flex-shrink-0 h-10 w-10">
-        <img className="h-10 w-10 rounded-full" src={row.original[column.imgAccessor]} alt="" />
-      </div>
-      <div className="ml-4">
-        <div className="text-sm font-medium text-gray-900">{value}</div>
-        <div className="text-sm text-gray-500">{row.original[column.emailAccessor]}</div>
-      </div>
-    </div>
   )
 }
 
